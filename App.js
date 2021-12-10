a4d9a495a74218502bdb60e1eefa18ef46866189
Miia Aapajärvi <miia.aapajarvi@haaga-helia.fi>
2021-12-10 12:18:28 +0200

    Eka osa valmis

--- App.js
+++ App.js
@@ -1,16 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';  
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import HomeScreen from './HomeScreen'
+import YogaPositions from './YogaPositions';
+import YogaTypes from './YogaTypes';
+
+
+const screenOptions = ({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+
+    if (route.name === 'Home') {
+      iconName = 'home';
+    } else if (route.name === 'Yoga Positions') {
+      iconName = 'yoga';
+    } else if (route.name === 'Yoga Types') {
+      iconName = 'yin-yang';
+    }
+
+    return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+  }
+});
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Yoga Positions" component={YogaPositions} />
+        <Tab.Screen name="Yoga Types" component={YogaTypes} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+export default function SettingScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });  
